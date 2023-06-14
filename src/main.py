6fec6ea789a626c0013b13065bc26ec00262f8ac
Orumbus <ElmeriG@gmail.com>
2023-06-14 16:18:46 +0300

    User improvements

--- src/main.py
+++ src/main.py
@@ -6,23 +6,27 @@ def main():
 
     :return:
     """
-    tensor, syllables = construct_data(samples=1250)
+
+    print("Constructing training data...")
+    tensor, syllables = construct_data()
+
+    print("Beginning training...")
     model, history = netTrain(tensor=tensor,syllables=syllables, epochs=100, batch_size=32, n_channels=32)
     del tensor, syllables
 
+    print("Constructing testing data...")
     tensor, syllables = construct_data(languages=["Estonian"], test=True, root=testing_loc)
-    mae, mape = netTest(tensor=tensor, syllables=syllables, model=model, batch_size=32)
+
+    print("Beginning training...")
+    netTest(tensor=tensor, syllables=syllables, model=model, batch_size=32)
     del tensor, syllables
 
-    print("Estonian MAE:", mae)
-    print("Estonian MAPE:", mape)
-
+    print("Constructing testing data...")
     tensor, syllables = construct_data(languages=["English"], test=True, root=testing_loc)
-    mae, mape = netTest(tensor=tensor, syllables=syllables, model=model, batch_size=32)
-    del tensor, syllables
 
-    print("English MAE:", mae)
-    print("English MAPE:", mape)
+    print("Beginning training...")
+    netTest(tensor=tensor, syllables=syllables, model=model, batch_size=32)
+    del tensor, syllables
 
     return
 
